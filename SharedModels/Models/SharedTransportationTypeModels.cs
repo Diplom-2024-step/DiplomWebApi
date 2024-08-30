1fa1881661b9fc63253dbb4e6e6e66a0a2cd17d7
MariykoVladislav <gghg135123@gmail.com>
2024-08-31 01:16:23 +0300

    Create TransportationType full model

---
+++ SharedModels/Models/SharedTransportationTypeModels.cs
@@ -0,0 +1,40 @@
+﻿using AnytourApi.Domain.Models.Enteties;
+using AnytourApi.Dtos.Dto.Models.TransportationTypes;
+using AnytourApi.SharedModels.Shared;
+
+namespace AnytourApi.SharedModels.Models;
+
+public class SharedTransportationTypeModels : SharedModelsBase, IShareModels<CreateTransportationTypeDto, UpdateTransportationTypeDto, TransportationType>
+{
+    public static TransportationType GetSample()
+    {
+        return new TransportationType()
+        {
+            Name = "Name",
+        };
+    }
+
+    public static CreateTransportationTypeDto GetSampleCreateDto()
+    {
+        return new CreateTransportationTypeDto()
+        {
+            Name = "test",
+        };
+    }
+
+    public static TransportationType GetSampleForUpdate()
+    {
+        return new TransportationType()
+        {
+            Name = "Name123"
+        };
+    }
+
+    public static UpdateTransportationTypeDto GetSampleUpdateDto()
+    {
+        return new UpdateTransportationTypeDto()
+        {
+            Name = "test12",
+        };
+    }
+}
