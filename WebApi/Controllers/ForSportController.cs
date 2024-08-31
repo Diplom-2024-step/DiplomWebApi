fe1fba5daa69755e0d194b3a87337ac9c3063003
MariykoVladislav <gghg135123@gmail.com>
2024-08-31 17:39:26 +0300

    Create TransportationType full model
    add Controllers

---
+++ WebApi/Controllers/ForSportController.cs
@@ -0,0 +1,14 @@
+﻿using AnytourApi.Application.Services.Models.ForSports;
+using AnytourApi.Domain.Models.Enteties;
+using AnytourApi.Dtos.Dto.Models.ForSports;
+using AnytourApi.WebApi.Shared;
+
+namespace AnytourApi.WebApi.Controllers;
+
+public class ForSportController(IForSportService CrudService, IHttpContextAccessor HttpContextAccessor) : CrudController<
+    GetForSportDto,
+    UpdateForSportDto,
+    CreateForSportDto,
+    IForSportService,
+    ForSport,
+    GetForSportDto>(CrudService, HttpContextAccessor);
