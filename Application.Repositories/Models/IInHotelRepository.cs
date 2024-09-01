fd02159cee2674b1299832cffad298f1476e7df7
Black foxi <dentef767@gmail.com>
2024-09-01 11:08:39 +0100

    Fix problem with DI for InHotelRepository

--- Application.Repositories/Models/IInHotelRepository.cs
+++ Application.Repositories/Models/IInHotelRepository.cs
@@ -3,4 +3,4 @@ using AnytourApi.Domain.Models.Enteties;
 
 namespace AnytourApi.Application.Repositories.Models;
 
-public interface IInHotelsRepository : ICrudRepository<InHotel>;
+public interface IInHotelRepository : ICrudRepository<InHotel>;
