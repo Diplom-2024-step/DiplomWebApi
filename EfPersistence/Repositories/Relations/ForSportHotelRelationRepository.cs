f3d54e5783c46e706edc57742f2fc202ae4396c9
MariykoVladislav <gghg135123@gmail.com>
2024-09-08 01:51:29 +0300

    Create ForSportHotel full models

---
+++ EfPersistence/Repositories/Relations/ForSportHotelRelationRepository.cs
@@ -0,0 +1,10 @@
+﻿using AnytourApi.Application.Repositories.Relations;
+using AnytourApi.Domain.Models.Enteties;
+using AnytourApi.Domain.Models.Relations;
+using AnytourApi.EfPersistence.Data;
+
+namespace AnytourApi.EfPersistence.Repositories.Relations;
+
+public class ForSportHotelRelationRepository(AppDbContext dbContext) : 
+    RelationRepository<ForSportHotel, ForSport, Hotel>(dbContext),
+    IForSportHotelRelationRepository;
---
+++ UnitTests/Repositories/Relations/ForSportHotelRelationRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using AnytourApi.Application.Repositories.Models;
+using AnytourApi.Application.Repositories.Relations;
+using AnytourApi.Domain.Models.Enteties;
+using AnytourApi.Domain.Models.Relations;
+using AnytourApi.EfPersistence.Data;
+using AnytourApi.EfPersistence.Repositories.Models;
+using AnytourApi.EfPersistence.Repositories.Relations;
+using AnytourApi.SharedModels.Models;
+using AnytourApi.UnitTests.Shared.Repositories;
+using Xunit.Sdk;
+
+namespace AnytourApi.UnitTests.Repositories.Relations;
+
+public class ForSportHotelRelationRepositoryTest : SharedRelationRepositoryTest<
+    ForSportHotel, ForSport, Hotel,
+    IForSportHotelRelationRepository, IForSportRepository, IHotelRepository>
+{
+    public override ForSport GetFirstModel()
+    {
+        return SharedForSportModels.GetSample();
+    }
+
+    public override IForSportRepository GetFirstRepository(AppDbContext appDbContext)
+    {
+        return new ForSportRepository(appDbContext);
+    }
+
+    public override ForSportHotel GetRelationModel(Guid firstId, Guid secondId)
+    {
+        return new ForSportHotel()
+        {
+            FirstId = firstId,
+            SecondId = secondId
+        };
+    }
+
+    public override IForSportHotelRelationRepository GetRelationRepository(AppDbContext appDbContext)
+    {
+        return new ForSportHotelRelationRepository(appDbContext);
+    }
+
+    public override Hotel GetSecondModel()
+    {
+        return SharedHotelModels.GetSample();
+    }
+
+    public override IHotelRepository GetSecondRepository(AppDbContext appDbContext)
+    {
+        return new HotelRepository(appDbContext);
+    }
+}
