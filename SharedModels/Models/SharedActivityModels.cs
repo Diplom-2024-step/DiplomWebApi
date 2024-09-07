cb5d103d6dca920fa03a7f86fc2df6b068bb73a7
MariykoVladislav <gghg135123@gmail.com>
2024-09-07 17:46:11 +0300

    Add Activity full models

--- SharedModels/Models/SharedActivityModels.cs
+++ SharedModels/Models/SharedActivityModels.cs
@@ -1,11 +1,30 @@
-﻿using AnytourApi.Domain.Models.Enteties;
+﻿using AnytourApi.Application.Repositories.Models;
+using AnytourApi.Application.Services.Models.Activities;
+using AnytourApi.Domain.Models.Enteties;
 using AnytourApi.Dtos.Dto.Models.Activities;
+using AnytourApi.EfPersistence.Repositories.Models;
 using AnytourApi.SharedModels.Shared;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AnytourApi.SharedModels.Models;
 
 public class SharedActivityModels : SharedModelsBase, IShareModels<CreateActivityDto, UpdateActivityDto, Activity>
 {
+    public static void AddAllDependencies(IServiceCollection services)
+    {
+        services.AddScoped<IActivityRepository, ActivityRepository>();
+
+        services.AddScoped<IActivityService, ActivityService>();
+    }
+
+    public static async Task<Guid> CreateModelWithAllDependenciesAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken)
+    {
+        var activity = SharedActivityModels.GetSampleCreateDto();
+
+        return await serviceProvider.GetService<IActivityService>().CreateAsync(activity, cancellationToken);
+
+    }
+
     public static Activity GetSample()
     {
         return new Activity()
