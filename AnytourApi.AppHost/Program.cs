9e91151b2cab5c1593fc6dd66457f3b54da01799
Black foxi <dentef767@gmail.com>
2024-09-30 19:53:59 +0100

    Add  cors changes

--- AnytourApi.AppHost/Program.cs
+++ AnytourApi.AppHost/Program.cs
@@ -4,12 +4,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<Projects.AnytourApi_WebApi>("anytourapi-webapi");
 
-builder.AddNpmApp("diplom-website", "../diplom-website", "dev")
-    .WithEnvironment("NODE_ENV", "development")
-    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
-    .WithReference(webapi)
-    .WithHttpEndpoint(env: "3000", name: "website", port: 3000, isProxied: false)
-    .WithEnvironment("NEXT_PUBLIC_BASE_URL", webapi.GetEndpoint("http") )
-    .WithExternalHttpEndpoints();
+//builder.AddNpmApp("diplom-website", "../diplom-website", "dev")
+//    .WithEnvironment("NODE_ENV", "development")
+//    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+//    .WithReference(webapi)
+//    .WithHttpEndpoint(env: "3000", name: "website", port: 3000, isProxied: false)
+//    .WithEnvironment("NEXT_PUBLIC_BASE_URL", webapi.GetEndpoint("http") )
+//    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
---
+++ WebApi/Middlewares/OptionsMiddleware.cs
@@ -0,0 +1,38 @@
+﻿namespace AnytourApi.WebApi.Middlewares;
+
+public class CorsMiddleware
+{
+    private readonly RequestDelegate _next;
+    private const string AllowedOrigins = "*"; // Or specify allowed origins
+
+    public CorsMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        await HandleCors(context);
+
+        await _next(context);
+    }
+
+    private async Task HandleCors(HttpContext context)
+    {
+        var origin = context.Request.Headers["Origin"].FirstOrDefault();
+
+        if (!string.IsNullOrEmpty(origin))
+        {
+            context.Response.Headers.Append("Access-Control-Allow-Origin", new[] { AllowedOrigins });
+            context.Response.Headers.Append("Access-Control-Allow-Credentials", new[] { "true" });
+
+            if (context.Request.Method == "OPTIONS")
+            {
+                context.Response.Headers.Append("Access-Control-Allow-Methods", new[] { "GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS" });
+                context.Response.Headers.Append("Access-Control-Allow-Headers", new[] { "Origin, X-Requested-With, Content-Type, Accept, Authorization" });
+                context.Response.StatusCode = 204; // No content
+                return;
+            }
+        }
+    }
+}
