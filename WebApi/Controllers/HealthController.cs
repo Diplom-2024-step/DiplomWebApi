ca8571b8a4bb0c2cfab62ee260b8217d4dde6ce9
Black foxi <dentef767@gmail.com>
2025-01-06 17:36:18 +0000

    add healthCheckEndPOint

---
+++ WebApi/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using AnytourApi.WebApi.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnytourApi.WebApi.Controllers;
+
+public class HealthController : MyBaseController
+{
+    public HealthController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+    {
+    }
+
+    [HttpGet("check")]
+    [AllowAnonymous]
+    public IActionResult Get(CancellationToken cancellationToken) => Ok();
+
+}
